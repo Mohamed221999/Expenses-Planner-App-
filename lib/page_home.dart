18aad588476547659980ba82a9a59c94e817ccfa
Mohamed Ragab <38363762+Mohamed221999@users.noreply.github.com>
2021-03-23 15:57:33 +0200

    split the app into multiple widgets

--- lib/page_home.dart
+++ lib/page_home.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
-import 'package:intl/intl.dart';
+import './widgets/transaction_list.dart';
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
-    //list of transaction
-    Transaction(
-        id: 't1', title: 'New Shose', amount: 79.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 55.25,
-        date: DateTime.now()),
-  ];
+
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
@@ -25,7 +15,7 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          //==========================Chart Part
+          //==============================Chart Part
           Container(
             width: double.infinity,
             child: Card(
@@ -54,57 +44,9 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          //==============================Transaction Part
-          //i will convert the list of transaction to a list of widet (Mappin) by using .map method and .toList method
-          Column(
-            children: transaction.map((e) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      margin: EdgeInsets.only(
-                          left: 10, top: 15, bottom: 15, right: 10),
-                      decoration:
-                          BoxDecoration(border: Border.all(color: Colors.pink)),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 8,
-                          vertical: 15,
-                        ),
-                        child: Center(
-                          child: Text(
-                            "\$" + e.amount.toString(),
-                            style: TextStyle(color: Colors.pink, fontSize: 17),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          e.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 17),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.01,
-                        ),
-                        //using intl package to formate the date============
-                        Text(
-                          DateFormat.yMMMd().format(e.date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-        ],
-      ),
+          TransactionList(),
+         ] 
+         ),   
     );
   }
 }
